df432f72c07d6c2bd6610cbf68a7c372b3d523c5
Alexandre Flamant <alexflamant@gmail.com>
2019-08-15 16:25:24 +0100

    up until delete section

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "cocktails", to: "cocktails#index", as: :cocktails
-  get 'cocktails/new', to: "cocktails#new", as: :new_cocktail
-  post "cocktails", to: "cocktails#create", as: :create_cocktail
-  get "cocktails/:id", to: "cocktails#show", as: :cocktail
-
+  resources :cocktails do
+    resources :doses, only: [:new, :create]
+  end
+  # get 'cocktails', to: 'cocktails#index', as: :cocktails
+  # get 'cocktails/new', to: 'cocktails#new', as: :new_cocktail
+  # post 'cocktails', to: 'cocktails#create', as: :create_cocktail
+  # get 'cocktails/:id', to: 'cocktails#show', as: :cocktail
 end
